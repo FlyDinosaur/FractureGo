ae92475bdaf3117f63b878ef809664957d488227
FlyDinosaur <3457552420@qq.com>
2025-06-17 23:28:23 +0800

    Initial Commit

---
+++ FractureGo/FractureGoApp.swift
@@ -0,0 +1,20 @@
+//
+//  FractureGoApp.swift
+//  FractureGo
+//
+//  Created by FlyDinosaur on 2025/6/17.
+//
+
+import SwiftUI
+
+@main
+struct FractureGoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
